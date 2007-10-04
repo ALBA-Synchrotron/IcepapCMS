674f3082dd94bd079f866c8a02b75761476b1b44
jribas <jribas@cells.es>
2007-10-04 12:51:52 +0000

    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@1664 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/lib_icepapcms/icepapdrivertemplate.py
+++
@@ -1,7 +0,0 @@
-from persistent import Persistent
-
-class IcepapDriverTemplate(Persistent):
-    def __init__(self, name, description, cfg):
-        self.name = name
-        self.description = description
-        self.cfg = cfg
