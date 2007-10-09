4f1ddda423d28fef03fc8214bd29d0631a6af590
jribas <jribas@a18efa20-c439-0410-bf20-9b91ec695b4c>
2007-10-09 01:34:36 +0000

    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@1727 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/qt_designer_files/ui_dialogaddlocation.py
+++ ui_icepapcms/qt_designer_files/ui_dialogaddlocation.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialogaddlocation.ui'
 #
-# Created: Mon Oct  8 18:22:28 2007
+# Created: Tue Oct  9 03:36:50 2007
 #      by: PyQt4 UI code generator 4.2
 #
 # WARNING! All changes made in this file will be lost!
--- ui_icepapcms/qt_designer_files/ui_pageipapdriver.py
+++ ui_icepapcms/qt_designer_files/ui_pageipapdriver.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'pageipapdriver.ui'
 #
-# Created: Mon Oct  8 18:22:29 2007
+# Created: Tue Oct  9 03:36:51 2007
 #      by: PyQt4 UI code generator 4.2
 #
 # WARNING! All changes made in this file will be lost!
