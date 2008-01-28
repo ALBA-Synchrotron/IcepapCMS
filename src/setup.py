4bf7d765712c33266ba29da4ab7e12ac5bc768cc
gcuni <gcuni@cells.es>
2008-01-28 07:11:09 +0000

    extra libs for py2exe included in this config file
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@2526 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-
+DISTUTILS_DEBUG=1
 from distutils.core import setup
 import glob
 import os
 
-
 if os.name == 'nt': #sys.platform == 'win32':
     import py2exe
-	
 
 setup(name = "icepapcms",
       version = "0.5.5",
@@ -15,11 +13,13 @@ setup(name = "icepapcms",
       author = "Guifre Cuni",
       author_email = "gcuni@cells.es",
       url = "http://www.cells.es",
+      #packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms/PyQt4", "lib_icepapcms/serial", "lib_icepapcms/pyIcePAP", "lib_icepapcms.icepapzodb", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.Led"],
       packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms.icepapzodb", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.Led"],
       data_files = [('doc', glob.glob("doc/*.*")),
                     ('db', glob.glob("db/*.sql")),
-                    ('templates', glob.glob("templates/*.*")),
+                    ('templates', glob.glob("templates/*.*"))
                     ],
+      
       scripts = ["icepapcms","icepapcms.py"],
 	  console = ["icepapcms.py"],
 	  windows = [
@@ -28,7 +28,5 @@ setup(name = "icepapcms",
             "icon_resources": [(1, "icepapcms.ico")]
         }
       ],
-      long_description = """
-IcepapCfg is a configuration and test tool for the Icepap motor controller
-"""          
-      )    
+      long_description = "IcepapCfg is a configuration and test tool for the Icepap motor controller",
+      options={"py2exe":{"includes":["sip","PyQt4._qt","serial","pyIcePAP"]}})
