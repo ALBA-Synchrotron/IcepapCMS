4d79a04f92ab58ace1aaa537db9f9e32d017f0a0
gcuni <gcuni@cells.es>
2008-02-20 14:41:26 +0000

    compiled uis in the parent directory
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@2893 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/ui_icepapcms/qt_designer_files/makepy.sh
+++ src/ui_icepapcms/qt_designer_files/makepy.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #rm *.py
 #rm *~
-pyrcc4 icepapcms.qrc -o qrc_icepapcms.py
-/usr/bin/pyuic4  ipaptestpage.ui -o ui_ipaptestpage.py
-/usr/bin/pyuic4  dialogaddicepap.ui -o ui_dialogaddicepap.py
-/usr/bin/pyuic4  dialogaddlocation.ui -o ui_dialogaddlocation.py
-/usr/bin/pyuic4  dialogsystemscan.ui -o ui_dialogsystemscan.py
-/usr/bin/pyuic4  dialogdriverconflict.ui -o ui_dialogdriverconflict.py
-/usr/bin/pyuic4  dialoghistoriccfg.ui -o ui_dialoghistoriccfg.py
-/usr/bin/pyuic4  dialogpreferences.ui -o ui_dialogpreferences.py
-/usr/bin/pyuic4  dialogipapprogram.ui -o ui_dialogipapprogram.py
-/usr/bin/pyuic4  pageipapdriver.ui -o ui_pageipapdriver.py
-/usr/bin/pyuic4  icepapcms.ui -o ui_icepapcms.py
-/usr/bin/pyuic4  ipapconsole.ui -o ui_ipapconsole.py
-/usr/bin/pyuic4  historiccfgwidget.ui -o ui_historiccfgwidget.py
+pyrcc4 icepapcms.qrc -o ../qrc_icepapcms.py
+/usr/bin/pyuic4  ipaptestpage.ui -o ../ui_ipaptestpage.py
+/usr/bin/pyuic4  dialogaddicepap.ui -o ../ui_dialogaddicepap.py
+/usr/bin/pyuic4  dialogaddlocation.ui -o ../ui_dialogaddlocation.py
+/usr/bin/pyuic4  dialogsystemscan.ui -o ../ui_dialogsystemscan.py
+/usr/bin/pyuic4  dialogdriverconflict.ui -o ../ui_dialogdriverconflict.py
+/usr/bin/pyuic4  dialoghistoriccfg.ui -o ../ui_dialoghistoriccfg.py
+/usr/bin/pyuic4  dialogpreferences.ui -o ../ui_dialogpreferences.py
+/usr/bin/pyuic4  dialogipapprogram.ui -o ../ui_dialogipapprogram.py
+/usr/bin/pyuic4  pageipapdriver.ui -o ../ui_pageipapdriver.py
+/usr/bin/pyuic4  icepapcms.ui -o ../ui_icepapcms.py
+/usr/bin/pyuic4  ipapconsole.ui -o ../ui_ipapconsole.py
+/usr/bin/pyuic4  historiccfgwidget.ui -o ../ui_historiccfgwidget.py
 
 
 #gedit ui_icepapdriverwidget.py &
