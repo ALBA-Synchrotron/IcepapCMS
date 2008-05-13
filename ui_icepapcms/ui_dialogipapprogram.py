32c264392dfa3c3d45c341aaec71b08a459df527
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-05-13 10:07:15 +0000

    new compiled version of the ui files
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@3609 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/ui_dialogipapprogram.py
+++ ui_icepapcms/ui_dialogipapprogram.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'dialogipapprogram.ui'
 #
-# Created: Tue Feb 26 10:32:26 2008
-#      by: PyQt4 UI code generator 4.3
+# Created: Tue May 13 11:51:30 2008
+#      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
 
