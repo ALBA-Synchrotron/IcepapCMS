0f01602931cf44392ccd3e0f4b479cb5de6237c9
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-10-02 13:22:59 +0000

    CLOOPENC -> PCLOOP in main tab
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@4821 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/qrc_icepapcms.py
+++ ui_icepapcms/qrc_icepapcms.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Oct 1 11:19:17 2008
+# Created: Thu Oct 2 15:16:08 2008
 #      by: The Resource Compiler for PyQt (Qt v4.3.4)
 #
 # WARNING! All changes made in this file will be lost!
