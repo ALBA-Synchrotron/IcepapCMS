2a95d21eb3bf89a0e8de9c460aa760bed55471bb
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-10-22 06:52:01 +0000

    no particular 4.3 method layout.setContentMargins
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@4948 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
+++ ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidgetsmall.ui'
 #
-# Created: Wed Oct 15 14:38:37 2008
+# Created: Tue Oct 21 11:55:04 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
