e9d702bfd2489ddc4fbaa861da4e1dd42e6e8748
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-10-27 17:31:23 +0000

    Driver->Set Expert Flag is now checkable
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@5014 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/qrc_icepapcms.py
+++ ui_icepapcms/qrc_icepapcms.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Tue Oct 21 11:54:57 2008
+# Created: Mon Oct 27 13:00:42 2008
 #      by: The Resource Compiler for PyQt (Qt v4.3.4)
 #
 # WARNING! All changes made in this file will be lost!
