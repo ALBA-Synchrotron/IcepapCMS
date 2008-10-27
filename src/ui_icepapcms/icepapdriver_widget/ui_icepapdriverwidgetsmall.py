ebddccfc5a3b5a3ca42a3cfbbae3cf01ecdf9c3a
gcuni <gcuni@cells.es>
2008-10-27 17:31:14 +0000

    Driver->Set Expert Flag is now checkable
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@5013 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
+++ src/ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidgetsmall.ui'
 #
-# Created: Tue Oct 21 11:55:04 2008
+# Created: Mon Oct 27 13:00:48 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
