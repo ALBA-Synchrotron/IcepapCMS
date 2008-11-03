711113f75ade1bb79d837b5c6d787e47b93f6e3e
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2008-11-03 14:54:08 +0000

    more preferences
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@5086 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/ui_dialogipapprogram.py
+++ ui_icepapcms/ui_dialogipapprogram.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialogipapprogram.ui'
 #
-# Created: Mon Oct 27 13:00:46 2008
+# Created: Mon Nov  3 12:19:01 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
--- ui_icepapcms/ui_ipaptestpage.py
+++ ui_icepapcms/ui_ipaptestpage.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ipaptestpage.ui'
 #
-# Created: Mon Oct 27 13:00:43 2008
+# Created: Mon Nov  3 12:18:56 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
