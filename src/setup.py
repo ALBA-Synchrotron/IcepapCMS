4064da01ff99f379afa17358860fb61099b036f5
gcuni <gcuni@cells.es>
2008-12-12 15:41:43 +0000

    more libraries for the setup for windows
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@5379 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/setup.py
+++ src/setup.py
@@ -8,13 +8,14 @@ if os.name == 'nt': #sys.platform == 'win32':
     import py2exe
 
 setup(name = "icepapcms",
-      version = "0.5.5",
+      version = "1.16",
       description = "Icepap Configuration Management System and Test Tool",
       author = "Guifre Cuni",
       author_email = "gcuni@cells.es",
       url = "http://www.cells.es",
       #packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms/PyQt4", "lib_icepapcms/serial", "lib_icepapcms/pyIcePAP", "lib_icepapcms.icepapzodb", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.Led"],
-      packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms.icepapzodb", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.Led"],
+      #packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms.icepapzodb", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.Led"],
+      packages = ["storm", "storm.databases", "lib_icepapcms", "lib_icepapcms.icepapzodb", "lib_icepapcms.IPy", "ui_icepapcms", "ui_icepapcms.icepapdriver_widget", "ui_icepapcms.icepapdriver_widget.Led", "ui_icepapcms.Led"],
       data_files = [('doc', glob.glob("doc/*.*")),
                     ('db', glob.glob("db/*.sql")),
                     ('templates', glob.glob("templates/*.*"))
