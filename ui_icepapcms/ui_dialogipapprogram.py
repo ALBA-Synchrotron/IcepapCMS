a1ab57dc0a5bd06cab30745254daf2d862e848b0
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2009-12-17 11:01:38 +0000

    new ui version with new homing flag -> this breaks previous version homing flag
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@8312 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/ui_dialogipapprogram.py
+++ ui_icepapcms/ui_dialogipapprogram.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialogipapprogram.ui'
 #
-# Created: Mon Nov  9 14:12:35 2009
+# Created: Thu Dec 17 11:52:46 2009
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
