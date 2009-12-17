ed9204910192ee6afb6640e411ef1a457a1df974
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2009-12-17 11:01:59 +0000

    new ui version with new homing flag -> this breaks previous version homing flag
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@8313 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
+++ ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidgetsmall.ui'
 #
-# Created: Mon Nov  9 14:12:42 2009
+# Created: Thu Dec 17 11:52:53 2009
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
