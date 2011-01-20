a9ebbe5bea268488c1023ec5056d1e2b427cddd4
gcuni <gcuni@cells.es>
2011-01-20 09:35:24 +0000

    Changes in save config button text
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@9908 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/ui_icepapcms/ui_dialogaddicepap.py
+++ src/ui_icepapcms/ui_dialogaddicepap.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialogaddicepap.ui'
 #
-# Created: Tue Jul 20 17:24:07 2010
+# Created: Thu Jan 20 10:55:45 2011
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
--- src/ui_icepapcms/ui_dialoghistoriccfg.py
+++ src/ui_icepapcms/ui_dialoghistoriccfg.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialoghistoriccfg.ui'
 #
-# Created: Tue Jul 20 17:24:09 2010
+# Created: Thu Jan 20 10:55:46 2011
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
