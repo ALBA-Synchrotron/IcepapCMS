82d5965b9cec94d1a0289d9b6546d8f03b8a1959
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2011-01-20 09:35:55 +0000

    Changes in save config button text
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@9909 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidget.py
+++ ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidget.ui'
 #
-# Created: Tue Jul 20 17:24:16 2010
+# Created: Thu Jan 20 10:55:54 2011
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
