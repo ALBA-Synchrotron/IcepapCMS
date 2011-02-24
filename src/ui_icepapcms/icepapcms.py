9013a8e012863dff6f3d7a3ee623db60e0cc866d
gcuni <gcuni@cells.es>
2011-02-24 09:46:00 +0000

    now developing for version 1.24
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@10020 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/ui_icepapcms/icepapcms.py
+++ src/ui_icepapcms/icepapcms.py
@@ -24,7 +24,7 @@ from templatescatalogwidget import TemplatesCatalogWidget
 #from dialogtemplate import DialogTemplate
 from optparse import OptionParser
 
-__version__ = "1.23"
+__version__ = "1.24"
 
 class IcepapApp(QtGui.QApplication):    
     def __init__(self, *args):
