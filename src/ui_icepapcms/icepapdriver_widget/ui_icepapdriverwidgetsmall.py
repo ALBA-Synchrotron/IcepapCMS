96aa64756a29454e6b4770a6d0d19a39aa708d9c
gcuni <gcuni@cells.es>
2014-02-26 17:28:22 +0000

    new ui version with small changes
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS@12362 a18efa20-c439-0410-bf20-9b91ec695b4c

--- src/ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
+++ src/ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidgetsmall.ui'
 #
-# Created: Thu Jan 20 10:55:54 2011
+# Created: Wed Feb 26 18:21:33 2014
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
