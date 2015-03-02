4f13870932a9ec56946b5771ffcd3c84f743129d
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2015-03-02 20:11:33 +0000

    define minimum size to avoid fields/labels overlapping
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@13370 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
+++ ui_icepapcms/icepapdriver_widget/ui_icepapdriverwidgetsmall.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'icepapdriver_widget/icepapdriverwidgetsmall.ui'
 #
-# Created: Thu Dec 11 19:47:20 2014
+# Created: Mon Mar  2 21:04:11 2015
 #      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
