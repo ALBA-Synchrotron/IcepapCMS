4ab5f97cf3b9689f189bbcc7d0684b320b909a54
gcuni <gcuni@a18efa20-c439-0410-bf20-9b91ec695b4c>
2015-05-28 22:22:43 +0000

    new version 1.32 with two small bugfixes: negative homepos and unkown in xml
    git-svn-id: file:///siciliarep/svn/trunk/miscellaneous/IcepapCMS/src@13459 a18efa20-c439-0410-bf20-9b91ec695b4c

--- ui_icepapcms/icepapcms.py
+++ ui_icepapcms/icepapcms.py
@@ -24,7 +24,7 @@ from templatescatalogwidget import TemplatesCatalogWidget
 #from dialogtemplate import DialogTemplate
 from optparse import OptionParser
 
-__version__ = "1.31"
+__version__ = "1.32"
 
 class IcepapApp(QtGui.QApplication):    
     def __init__(self, *args):
