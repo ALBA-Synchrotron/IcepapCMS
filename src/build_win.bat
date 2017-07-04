7b6e9c1ec19af68ca88127a84b943408bbecb292
cfalcon <cfalcon@cells.es>
2017-07-04 11:47:26 +0200

    Clean up code

--- src/build_win.bat
+++
@@ -1,5 +0,0 @@
-move setup.cfg setup.bak
-;python setup.py py2exe --includes sip,ExtensionClass
-; the includes are defined inside the setup.py configuration
-python setup.py py2exe
-move setup.bak setup.cfg
--- src/lib_icepapcms/IPy/test/test.rst
+++
@@ -1,4 +0,0 @@
->>> from IPy import IP
->>> IP('::ffff:1.2.3.4').strCompressed()
-'::ffff:1.2.3.4'
-
