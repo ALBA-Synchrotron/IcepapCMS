5c4b03e94b4a32c8f405e81760c6ba396b6fedeb
Jarkko Inki <jarkko.inki@maxiv.lu.se>
2018-12-07 12:20:11 +0100

    A working HOME/SRCH dialog.

--- icepapCMS/ui_icepapcms/dialoghomesrch.py
+++ icepapCMS/ui_icepapcms/dialoghomesrch.py
@@ -13,9 +13,9 @@
 
 from PyQt4.QtGui import QDialog
 from PyQt4.QtGui import QDialogButtonBox
+from PyQt4.QtCore import QTimer
 from ui_dialoghomesrch import Ui_DialogHomeSrch
 from messagedialogs import MessageDialogs
-#from ..lib_icepapcms import MainManager
 
 
 class DialogHomeSrch(QDialog):
@@ -27,14 +27,19 @@ class DialogHomeSrch(QDialog):
         self.parent = parent
         self.axis = axis
         self.close_button = self.ui.bbClose.button(QDialogButtonBox.Close)
-        self._connect_signals()
+        self.motor_moving = False
+        self._set_go_stop_btn_layout()
         self._cb_hs_changed()
+        self.tick_interval = 200  # [milliseconds]
+        self.ticker = QTimer()
+        self._connect_signals()
 
     def _connect_signals(self):
         self.ui.cbHomeSearch.currentIndexChanged.connect(self._cb_hs_changed)
         self.ui.cbHsOptions.currentIndexChanged.connect(self._update_access)
-        self.ui.btnGo.clicked.connect(self._move_motor)
+        self.ui.btnGoStop.clicked.connect(self._btn_go_stop_clicked)
         self.close_button.clicked.connect(self._close_dialog)
+        self.ticker.timeout.connect(self._tick_status)
 
     def _update_access(self):
         have_home = self.ui.cbHomeSearch.currentText() == 'HOME'
@@ -53,61 +58,89 @@ class DialogHomeSrch(QDialog):
         self.ui.cbHsOptions.addItems(items)
         self._update_access()
 
-    def _move_motor(self):
-        axis = self.axis
+    def _set_go_stop_btn_layout(self):
+        if self.motor_moving:
+            self.ui.btnGoStop.setStyleSheet("background-color: red")
+            self.ui.btnGoStop.setText('STOP')
+        else:
+            self.ui.btnGoStop.setStyleSheet("background-color: green")
+            self.ui.btnGoStop.setText('GO')
+
+    def _btn_go_stop_clicked(self):
+        if self.motor_moving:
+            self.motor_moving = False
+            self.axis.stop()
+        else:
+            self.motor_moving = True
+            self._start_motor()
+        self._set_go_stop_btn_layout()
+
+    def _start_motor(self):
         cmd = self.ui.cbHomeSearch.currentText()
         opt = self.ui.cbHsOptions.currentText()
         try:
             if cmd == 'HOME':
                 direction = int(opt)
-                axis.home(direction)
+                self.axis.home(direction)
             else:
                 edge = self.ui.cbEdge.currentText()
                 direction = self.ui.cbDirection.currentText()
-                axis.srch(opt, edge, direction)
+                self.axis.srch(opt, edge, direction)
         except RuntimeError as e:
             msg = 'HOME/SRCH failed:\n{}'.format(e)
             print(msg)
             MessageDialogs.showErrorMessage(None, 'HOME/SRCH', msg)
+            return
+        self.ticker.start(self.tick_interval)
 
-
-
-
-
-        #addr = axis.addr
-        #try:
-        #    if self.ui.cbHomeSearch.currentText() == 'HOME':
-        #        txt = '{}:?HOMESTAT\n'.format(addr)
-        #        status, direction = axis.homestat
-        #        f1 = axis.get_home_position
-        #        f2 = axis.get_home_encoder
-        #    else:
-        #        txt = '{}:?SRCHSTAT\n'.format(addr)
-        #        status, direction = axis.srchstat
-        #        f1 = axis.get_srch_position
-        #        f2 = axis.get_srch_encoder
-        #    if status == 'MOVING':
-        #        txt += 'Status: MOVING\n'
-        #        txt += 'Direction: {}\n'.format(direction)
-        #    elif status == 'NOTFOUND':
-        #        txt += 'Status: NOTFOUND\n'
-        #    else:
-        #        txt += 'Pos AXIS  : {}\n'.format(f1('AXIS'))
-        #        txt += 'Pos INPOS : {}\n'.format(f1('INPOS'))
-        #        txt += 'Pos TGTENC: {}\n'.format(f1('TGTENC'))
-        #        txt += 'Pos ENCIN : {}\n'.format(f1('ENCIN'))
-        #        txt += 'Pos ABSENC: {}\n'.format(f1('ABSENC'))
-        #        txt += 'Enc AXIS  : {}\n'.format(f2('AXIS'))
-        #        txt += 'Enc INPOS : {}\n'.format(f2('INPOS'))
-        #        txt += 'Enc TGTENC: {}\n'.format(f2('TGTENC'))
-        #        txt += 'Enc ENCIN : {}\n'.format(f2('ENCIN'))
-        #        txt += 'Enc ABSENC: {}\n'.format(f2('ABSENC'))
-        #    self.ui.homeBrowser.clear()
-        #    self.ui.homeBrowser.setText(txt)
-        #except RuntimeError as e:
-        #    msg = 'HOME/SRCH failed:\n{}'.format(e)
-        #    print(msg)
-        #    MessageDialogs.showErrorMessage(None, 'HOME/SRCH', msg)
+    def _tick_status(self):
+        try:
+            if self.ui.cbHomeSearch.currentText() == 'HOME':
+                status, direction = self.axis.homestat
+                f1 = self.axis.get_home_position
+                f2 = self.axis.get_home_encoder
+            else:
+                status, direction = self.axis.srchstat
+                f1 = self.axis.get_srch_position
+                f2 = self.axis.get_srch_encoder
+            if status == 'MOVING':
+                ss = "background-color: yellow"
+                self.ui.gvIndicator.setStyleSheet(ss)
+                if direction == -1:
+                    pass
+                elif direction == 1:
+                    pass
+                else:
+                    self.axis.stop()
+                    msg = 'Internal Error: Bad direction'
+                    print(msg)
+                    MessageDialogs.showErrorMessage(None, 'HOME/SRCH', msg)
+                    return
+                self.ticker.start(self.tick_interval)
+            elif status == 'NOTFOUND':
+                ss = "background-color: red"
+                self.ui.gvIndicator.setStyleSheet(ss)
+                self.motor_moving = False
+                self._set_go_stop_btn_layout()
+            else:
+                ss = "background-color: green"
+                self.ui.gvIndicator.setStyleSheet(ss)
+                self.motor_moving = False
+                self._set_go_stop_btn_layout()
+                self.ui.lcdPosAxis.display(f1('AXIS'))
+                self.ui.lcdPosTgtenc.display(f1('INPOS'))
+                self.ui.lcdPosEncin.display(f1('TGTENC'))
+                self.ui.lcdPosInpos.display(f1('ENCIN'))
+                self.ui.lcdPosAbsenc.display(f1('ABSENC'))
+                self.ui.lcdEncAxis.display(f2('AXIS'))
+                self.ui.lcdEncTgtenc.display(f2('INPOS'))
+                self.ui.lcdEncEncin.display(f2('TGTENC'))
+                self.ui.lcdEncInpos.display(f2('ENCIN'))
+                self.ui.lcdEncAbsenc.display(f2('ABSENC'))
+        except RuntimeError as e:
+            msg = 'HOME/SRCH failed:\n{}'.format(e)
+            print(msg)
+            MessageDialogs.showErrorMessage(None, 'HOME/SRCH', msg)
 
     def _close_dialog(self):
         self.parent.enable_home_srch_button()
