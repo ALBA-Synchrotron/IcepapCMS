2eb0ac0ce39735e941070c45469d175385be82de
Roberto Javier Homs Puron <rhoms@cells.es>
2019-03-20 16:18:26 +0100

    Bump version 2.3.5 to 2.3.6

--- icepapCMS/ui_icepapcms/icepapcms.py
+++ icepapCMS/ui_icepapcms/icepapcms.py
@@ -37,7 +37,7 @@ from templatescatalogwidget import TemplatesCatalogWidget
 #from dialogtemplate import DialogTemplate
 from optparse import OptionParser
 
-__version__ = '2.3.5'
+__version__ = '2.3.6'
 
 class IcepapApp(QtGui.QApplication):    
     def __init__(self, *args):
