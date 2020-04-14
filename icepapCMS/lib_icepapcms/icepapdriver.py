5428f2981a735d6c00c0515a2b669401c11aa2bb
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-14 17:21:18 +0200

    Pass flake8 to icepapdriver.py

--- icepapCMS/lib_icepapcms/icepapdriver.py
+++ icepapCMS/lib_icepapcms/icepapdriver.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
 
-# ------------------------------------------------------------------------------
-# This file is part of icepapCMS (https://github.com/ALBA-Synchrotron/icepapcms)
+# -----------------------------------------------------------------------------
+# This file is part of icepapCMS https://github.com/ALBA-Synchrotron/icepapcms
 #
 # Copyright 2008-2018 CELLS / ALBA Synchrotron, Bellaterra, Spain
 #
 # Distributed under the terms of the GNU General Public License,
 # either version 3 of the License, or (at your option) any later version.
 # See LICENSE.txt for more info.
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
-from storm.locals import *
-from .icepapdrivercfg import IcepapDriverCfg
+from storm.locals import Storm, Int, Unicode, Reference, ReferenceSet
 from .conflict import Conflict
 from .configmanager import ConfigManager
 from .icepapcontroller import IcepapController
 from .stormmanager import StormManager
-import os
 import time
 from datetime import datetime
 import socket
-from pyIcePAP import Mode
+from icepap import Mode
 
 
 class IcepapDriver(Storm):
@@ -33,30 +31,30 @@ class IcepapDriver(Storm):
     mode = Unicode()
     """ references """
     icepap_system = Reference(icepapsystem_name, "IcepapSystem.name")
-    historic_cfgs = ReferenceSet((icepapsystem_name, addr), ("IcepapDriverCfg.icepapsystem_name", "IcepapDriverCfg.driver_addr"))
+    historic_cfgs = ReferenceSet((icepapsystem_name, addr),
+                                 ("IcepapDriverCfg.icepapsystem_name",
+                                  "IcepapDriverCfg.driver_addr"))
 
-    
     def __init__(self, icepap_name, addr):
-        self.icepapsystem_name = str(icepap_name)        
+        self.icepapsystem_name = str(icepap_name)
         self.addr = addr
         self.current_cfg = None
         self.initialize()
-        
-    
+
     def __storm_loaded__(self):
         self.current_cfg = self.historic_cfgs.order_by("date").last()
         self.initialize()
-    
+
     def initialize(self):
         self.drivernr = self.addr % 10
-        self.cratenr = self.addr / 10 
+        self.cratenr = self.addr / 10
         self._undo_list = []
         self.startup_cfg = self.current_cfg
         self.conflict = Conflict.NO_CONFLICT
-        
-    def addConfiguration(self, cfg, current = True):
+
+    def addConfiguration(self, cfg, current=True):
         if current:
-            if not self.current_cfg is None:
+            if self.current_cfg is not None:
                 self._undo_list.append(self.current_cfg)
             else:
                 self.startup_cfg = cfg
@@ -66,40 +64,41 @@ class IcepapDriver(Storm):
 
     def setConflict(self, conflict):
         self.conflict = conflict
-        
+
     def getName(self):
         return self.name
 
     def setName(self, name):
         self.name = str(name)
-    
+
     def setMode(self, mode):
         self.mode = str(mode)
-    
+
     def signDriver(self):
-        # AS ESRF SAYS, WHEN SIGNING THE DRIVER CONFIG, THE COMMIT SHOULD BE DONE
-        # IN THE DATABASE FIRST, AND IF NO ERRORS, THEN COMMUNICATE THE DRIVER
-        # THAT THE VALUES SHOULD BE SIGNED.
+        # AS ESRF SAYS, WHEN SIGNING THE DRIVER CONFIG, THE COMMIT SHOULD
+        # BE DONE IN THE DATABASE FIRST, AND IF NO ERRORS, THEN COMMUNICATE
+        # THE DRIVER THAT THE VALUES SHOULD BE SIGNED.
         try:
             user = ConfigManager().username
             host = socket.gethostname()
-            signature = user+"@"+host+"_"+datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
-            IcepapController().signDriverConfiguration(self.icepapsystem_name, self.addr, signature)
+            signature = user + "@" + host + "_" + \
+                datetime.now().strftime('%Y/%m/%d_%H:%M:%S')
+            IcepapController().signDriverConfiguration(
+                self.icepapsystem_name, self.addr, signature)
             self.mode = str(Mode.OPER)
             db = StormManager()
             db.commitTransaction()
             self.current_cfg.name = str(time.ctime())
-            self.current_cfg.setSignature(signature)        
+            self.current_cfg.setSignature(signature)
             self.startup_cfg = self.current_cfg
             self.conflict = Conflict.NO_CONFLICT
         except Exception as e:
-            print("some exception while trying to sign the driver",e)
+            print("some exception while trying to sign the driver", e)
 
-    
     def setStartupCfg(self):
         self.current_cfg = self.startup_cfg
         self.conflict = Conflict.NO_CONFLICT
-    
+
     def undo(self, config):
         self.addConfiguration(config)
         # THE CURRENT CONFIGURATION SHOULD NOT BE IN THE UNDO LIST
@@ -107,15 +106,14 @@ class IcepapDriver(Storm):
 
     def getUndoList(self):
         return self._undo_list.pop()
-    
+
     def hasUndoList(self):
         return len(self._undo_list) > 0
-    
+
     def saveHistoricCfg(self, now, name, desc):
         self.current_cfg.name = str(name)
         self.current_cfg.description = str(desc)
 
-    
     def deleteHistoricCfg(self, cfg):
         self.historic_cfgs.remove(cfg)
 
@@ -127,9 +125,8 @@ class IcepapDriver(Storm):
         self.setConflict(Conflict.DRIVER_CHANGED)
 
         return -1
-     
 
     # TO SORT THE ICEPAP DRIVERS IN THE TREE
-    def __lt__(self,other):
-        if isinstance(other,IcepapDriver):
+    def __lt__(self, other):
+        if isinstance(other, IcepapDriver):
             return self.addr < other.addr
