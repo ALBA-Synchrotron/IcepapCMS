8a96d2504af64289ea346b594a7001994ba4737c
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-14 19:13:35 +0200

    Pass flake8 to timer.py

--- icepapCMS/lib_icepapcms/timer.py
+++ icepapCMS/lib_icepapcms/timer.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
-# ------------------------------------------------------------------------------
-# This file is part of icepapCMS (https://github.com/ALBA-Synchrotron/icepapcms)
+# -----------------------------------------------------------------------------
+# This file is part of icepapCMS https://github.com/ALBA-Synchrotron/icepapcms
 #
 # Copyright 2008-2018 CELLS / ALBA Synchrotron, Bellaterra, Spain
 #
 # Distributed under the terms of the GNU General Public License,
 # either version 3 of the License, or (at your option) any later version.
 # See LICENSE.txt for more info.
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 # TODO use threading module instead of _thread
 import _thread
 import time
 
-################################################################################
 
 class Timer:
 
