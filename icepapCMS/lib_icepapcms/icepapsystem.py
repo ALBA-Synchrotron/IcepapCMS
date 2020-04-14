9c9e7119028d1ba4593282e70fb682491a33e3d0
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-14 17:47:15 +0200

    Pass flake8 to icepapsystem.py

--- icepapCMS/lib_icepapcms/icepapsystem.py
+++ icepapCMS/lib_icepapcms/icepapsystem.py
@@ -1,44 +1,45 @@
 #!/usr/bin/env python
 
-# ------------------------------------------------------------------------------
-# This file is part of icepapCMS (https://github.com/ALBA-Synchrotron/icepapcms)
+# -----------------------------------------------------------------------------
+# This file is part of icepapCMS https://github.com/ALBA-Synchrotron/icepapcms
 #
 # Copyright 2008-2018 CELLS / ALBA Synchrotron, Bellaterra, Spain
 #
 # Distributed under the terms of the GNU General Public License,
 # either version 3 of the License, or (at your option) any later version.
 # See LICENSE.txt for more info.
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
-from storm.locals import *
+from storm.locals import Storm, Unicode, ReferenceSet, Int
 from .conflict import Conflict
-import sys
 from .stormmanager import StormManager
+from .icepapdriver import IcepapDriver
 
- 
+
 class Location(Storm):
     __storm_table__ = "location"
     name = Unicode(primary=True)
     systems = ReferenceSet(name, "IcepapSystem.location_name")
+
     def __init__(self, name):
         self.name = name
         self.initialize()
-        
+
     def initialize(self):
         self._inmemory_systems = {}
-        
+
     def __storm_loaded__(self):
         self.initialize()
 
     def loadSystemsfromDB(self):
         for system in self.systems:
             self._inmemory_systems[system.name] = system
-    
+
     def addSystem(self, system):
         self.systems.add(system)
         self._inmemory_systems[system.name] = system
-    
+
     def deleteSystem(self, name):
         system = self.systems.find(IcepapSystem.name == name).one()
         for driver in system.getDrivers():
@@ -47,10 +48,9 @@ class Location(Storm):
         try:
             del self._inmemory_systems[name]
             self._inmemory_drivers = {}
-        except:
+        except Exception:
             pass
-        
-            
+
 
 class IcepapSystem(Storm):
     __storm_table__ = "icepapsystem"
@@ -63,32 +63,30 @@ class IcepapSystem(Storm):
     """ references """
     drivers = ReferenceSet(name, "IcepapDriver.icepapsystem_name")
     location = ReferenceSet(location_name, "Location.name")
-    
-    def __init__(self, name, host, port, location_name, description = None):
+
+    def __init__(self, name, host, port, location_name, description=None):
         self.name = str(name)
-        if description == None:
+        if description is None:
             description = str("")
         self.description = str(description)
         self.location_name = location_name
         self.host = str(host)
         self.port = int(port)
         self.initialize()
-    
+
     def __storm_loaded__(self):
         self.initialize()
 
-        
     def loadDriversfromDB(self):
         for driver in self.drivers:
             self._inmemory_drivers[driver.addr] = driver
-        
-    
+
     def initialize(self):
         self._inmemory_drivers = {}
         self.conflict = Conflict.NO_CONFLICT
         self.child_conflicts = 0
-     
-    def getDriver(self, addr, in_memory = True):
+
+    def getDriver(self, addr, in_memory=True):
         if in_memory:
             if addr in self._inmemory_drivers:
                 return self._inmemory_drivers[addr]
@@ -96,45 +94,45 @@ class IcepapSystem(Storm):
                 return None
         else:
             return self.drivers.find(IcepapDriver.addr == addr).one()
-    
-    def getDrivers(self, in_memory = True):
-        #return self.drivers.order_by(IcepapDriver.addr)
+
+    def getDrivers(self, in_memory=True):
         if in_memory:
             if len(self._inmemory_drivers) == 0:
                 self.loadDriversfromDB()
             return list(self._inmemory_drivers.values())
         else:
             return self.drivers
-    
+
     def addDriver(self, driver):
         self.drivers.add(driver)
         self._inmemory_drivers[driver.addr] = driver
-        
+
     def addDriverList(self, driver_list):
         for driver in list(driver_list.values()):
             self.addDriver(driver)
-    
+
     def removeDriver(self, addr):
         driver = self.drivers.find(IcepapDriver.addr == addr).one()
         StormManager().deleteDriver(driver)
         del self._inmemory_drivers[addr]
-    
+
     def setConflict(self, conflict):
         self.conflict = conflict
-    
+
     def signSystem(self):
         for driver in self.getDrivers():
             driver.signDriver()
-    
+
     def signCrate(self, cratenr):
-        min = cratenr * 10
-        max =  (cratenr + 1)* 10         
-        for driver in self.drivers.find(IcepapDriver.addr > min, IcepapDriver.addr < max):
-            driver.signDriver()            
-              
-    def compareDriverList(self, driver_list):        
+        min_addr = cratenr * 10
+        max_addr = (cratenr + 1) * 10
+        for driver in self.drivers.find(IcepapDriver.addr > min_addr,
+                                        IcepapDriver.addr < max_addr):
+            driver.signDriver()
+
+    def compareDriverList(self, driver_list):
         self.child_conflicts = 0
-        conflictsList = []        
+        conflictsList = []
         self.conflict = Conflict.NO_CONFLICT
         ''' comparing drivers '''
         for driver in self.drivers:
@@ -144,7 +142,7 @@ class IcepapSystem(Storm):
                 self.child_conflicts += 1
             else:
                 driver_cmp = driver_list[addr]
-                if not driver == driver_cmp :
+                if not driver == driver_cmp:
                     # HOOK TO CHECK AUTO-SOLVE CONFLICTS
                     dsp_cfg = driver_cmp.current_cfg
                     db_cfg = driver.current_cfg
@@ -152,11 +150,11 @@ class IcepapSystem(Storm):
                     conflictsList.append([conflict, self, addr])
                     self.child_conflicts += 1
 
-        ''' checking for new drivers '''        
+        # checking for new drivers
         for addr, driver in list(driver_list.items()):
             if self.drivers.find(IcepapDriver.addr == addr).count() == 0:
                 self.addDriver(driver)
-                ''' determine if it is a new driver or if it has been moved '''
+                # determine if it is a new driver or if it has been moved
                 # ALWAYS TREAT AS NEW DRIVER, 'MOVED' HAS NO SENSE
                 conflictsList.append([Conflict.NEW_DRIVER, self, addr])
 
@@ -167,73 +165,75 @@ class IcepapSystem(Storm):
         # ISSUE 053 in WIKI MINUTES
         # http://wikiserv.esrf.fr/esl/index.php/IcePAP_minute_130708
         #
-        # TWO NEW CONFLICT TYPES ADDED: DRIVER_AUTOSOLVE, DRIVER_AUTOSOLVE_EXPERT
-        # Since there is the possibility to keep current behaviour, the method can return also DRIVER_CHANGED
+        # TWO NEW CONFLICT TYPES ADDED: DRIVER_AUTOSOLVE,
+        # DRIVER_AUTOSOLVE_EXPERT
+        # Since there is the possibility to keep current behaviour,
+        # the method can return also DRIVER_CHANGED
 
-        # NOTE: u'VER' and u'IPAPNAME' are also available to resolve conflicts...
+        # NOTE: u'VER' and u'IPAPNAME' are also available to resolve
+        # conflicts...
 
         # NOTE: configs have the .getParameter(par) method
-        #       BUT dsp values are not stored in the database, so in_memory has to be set to True
-        ### par = u'VER'
-        ### print 'dsp', par, dsp_cfg.getParameter(par, in_memory=True)
-        ### print 'db', par, db_cfg.getParameter(par)
+        #       BUT dsp values are not stored in the database, so in_memory
+        #       has to be set to True
+        # ## par = u'VER'
+        # ## print 'dsp', par, dsp_cfg.getParameter(par, in_memory=True)
+        # ## print 'db', par, db_cfg.getParameter(par)
 
         # NOTE: it is also possible to operate with lists:
-        ###dsp_values = dsp_cfg.toList()
-        ###db_values = db_cfg.toList()
-        ###for p,v in dsp_values:
-        ###    if p == par:
-        ###        print 'dsp', p, v
-        ###for p,v in db_values:
-        ###    if p == par:
-        ###        print 'db', p, v
+        # ##dsp_values = dsp_cfg.toList()
+        # ##db_values = db_cfg.toList()
+        # ##for p,v in dsp_values:
+        # ##    if p == par:
+        # ##        print 'dsp', p, v
+        # ##for p,v in db_values:
+        # ##    if p == par:
+        # ##        print 'db', p, v
 
         try:
-            dsp_cfg_ver = float(dsp_cfg.getParameter(str("VER"), 
+            dsp_cfg_ver = float(dsp_cfg.getParameter(str("VER"),
                                                      in_memory=True))
-        except:
+        except Exception:
             print("ERROR: missing VERsion parameter in DSP config")
             return Conflict.DRIVER_CHANGED
 
         try:
-            db_cfg_ver  = float(db_cfg.getParameter(str("VER")))
-        except:
+            db_cfg_ver = float(db_cfg.getParameter(str("VER")))
+        except Exception:
             print("ERROR: missing VERsion parameter in database config")
             return Conflict.DRIVER_CHANGED
 
         #
-        if((dsp_cfg_ver==2.0) and (db_cfg_ver<2.0) and (db_cfg_ver>=1.22)):
-            dsp_values  = dsp_cfg.toList()
-            db_values   = db_cfg.toList()
+        if(dsp_cfg_ver == 2.0) and (db_cfg_ver < 2.0) and (db_cfg_ver >= 1.22):
+            dsp_values = dsp_cfg.toList()
+            db_values = db_cfg.toList()
             diff_values = set(dsp_values).difference(db_values)
-            for p,v in diff_values:
+            for p, v in diff_values:
                 if p == 'VER':
-                   continue
+                    continue
 
-                if not p in ['EXTDISABLE','PCLMODE','EXTBUSY','POSUPDATE','LNKNAME','EXTPOWER','OUTPSRC']:
-                   return Conflict.DRIVER_CHANGED
+                if p not in ['EXTDISABLE', 'PCLMODE', 'EXTBUSY',
+                             'POSUPDATE', 'LNKNAME', 'EXTPOWER', 'OUTPSRC']:
+                    return Conflict.DRIVER_CHANGED
             return Conflict.DRIVER_AUTOSOLVE
 
-        #
-        if((dsp_cfg_ver>3.14) and (db_cfg_ver<=3.14) and (db_cfg_ver>=2.0)):
-            print("DSP VERSION: ",dsp_cfg_ver)
-            print("DB  VERSION: ",db_cfg_ver)
-            dsp_values  = dsp_cfg.toList()
-            db_values   = db_cfg.toList()
+        if(dsp_cfg_ver > 3.14) and (db_cfg_ver <= 3.14) and \
+                (db_cfg_ver >= 2.0):
+            print("DSP VERSION: ", dsp_cfg_ver)
+            print("DB  VERSION: ", db_cfg_ver)
+            dsp_values = dsp_cfg.toList()
+            db_values = db_cfg.toList()
             diff_values = set(dsp_values).difference(db_values)
             print("diff_values", diff_values)
-            for p,v in diff_values:
+            for p, v in diff_values:
                 # ignore new parameters or parameters that normally change
-                if ((p == 'VER') or (p == 'HOLDTIME') or (p == 'EXTHOLD')):
-                   continue
+                if (p == 'VER') or (p == 'HOLDTIME') or (p == 'EXTHOLD'):
+                    continue
 
                 # parameters which value is not backward compatible
-                if not p in ['INFOASRC','INFOBSRC','INFOCSRC']:
-                   return Conflict.DRIVER_CHANGED
+                if p not in ['INFOASRC', 'INFOBSRC', 'INFOCSRC']:
+                    return Conflict.DRIVER_CHANGED
 
             return Conflict.DRIVER_AUTOSOLVE_EXPERT
 
         return Conflict.DRIVER_CHANGED
-
-from .icepapdriver import IcepapDriver
-
