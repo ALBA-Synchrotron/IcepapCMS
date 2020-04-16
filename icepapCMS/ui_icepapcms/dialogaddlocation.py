9b1e73f141cbe107931d5b73111c8ba1c2d7f83c
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-16 16:31:14 +0200

    Pass flake8 to dialogaddlocation.py

--- icepapCMS/ui_icepapcms/dialogaddlocation.py
+++ icepapCMS/ui_icepapcms/dialogaddlocation.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
-# ------------------------------------------------------------------------------
-# This file is part of icepapCMS (https://github.com/ALBA-Synchrotron/icepapcms)
+# -----------------------------------------------------------------------------
+# This file is part of icepapCMS https://github.com/ALBA-Synchrotron/icepapcms
 #
 # Copyright 2008-2018 CELLS / ALBA Synchrotron, Bellaterra, Spain
 #
 # Distributed under the terms of the GNU General Public License,
 # either version 3 of the License, or (at your option) any later version.
 # See LICENSE.txt for more info.
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtGui
 from .ui_dialogaddlocation import Ui_DialogAddLocation
 from .qrc_icepapcms import *
 
@@ -21,11 +20,11 @@ class DialogAddLocation(QtGui.QDialog):
         QtGui.QDialog.__init__(self, parent)
         self.ui = Ui_DialogAddLocation()
         self.modal = True
-        self.ui.setupUi(self)        
+        self.ui.setupUi(self)
 
     def getData(self):
-        name = str(self.ui.txtName.text())        
+        name = str(self.ui.txtName.text())
         return name
+
     def setData(self, name):
         self.ui.txtname.setText(name)
-                
