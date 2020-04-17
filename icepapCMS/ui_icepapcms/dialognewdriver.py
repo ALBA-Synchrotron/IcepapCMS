6b1ee791ae6c3aff4d94d2369e0eae6671445ad6
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-17 09:00:18 +0200

    Pass flake8 to dialognewdriver.py

--- icepapCMS/ui_icepapcms/dialognewdriver.py
+++ icepapCMS/ui_icepapcms/dialognewdriver.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-# ------------------------------------------------------------------------------
-# This file is part of icepapCMS (https://github.com/ALBA-Synchrotron/icepapcms)
+# -----------------------------------------------------------------------------
+# This file is part of icepapCMS https://github.com/ALBA-Synchrotron/icepapcms
 #
 # Copyright 2008-2018 CELLS / ALBA Synchrotron, Bellaterra, Spain
 #
 # Distributed under the terms of the GNU General Public License,
 # either version 3 of the License, or (at your option) any later version.
 # See LICENSE.txt for more info.
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 
 from PyQt4 import QtCore, QtGui
 from .ui_dialognewdriver import Ui_DialogNewDriver
-from ..lib_icepapcms import MainManager
+
 
 class DialogNewDriver(QtGui.QDialog):
     def __init__(self, parent, more_info, expertFlag):
@@ -31,13 +31,23 @@ class DialogNewDriver(QtGui.QDialog):
             self.ui.btnUseDriver.setVisible(False)
         self.connectSignals()
 
-
     def connectSignals(self):
-        QtCore.QObject.connect(self.ui.btnUseDefaults,QtCore.SIGNAL("pressed()"),self.btnUseDefaults_clicked)
-        QtCore.QObject.connect(self.ui.btnUseDriver,QtCore.SIGNAL("pressed()"),self.btnUseDriver_clicked)
-        QtCore.QObject.connect(self.ui.btnCancel,QtCore.SIGNAL("pressed()"),self.btnCancel_clicked)
-        QtCore.QObject.connect(self.ui.btnMoreInfo,QtCore.SIGNAL("pressed()"),self.btnMoreInfo_clicked)
-
+        QtCore.QObject.connect(
+            self.ui.btnUseDefaults,
+            QtCore.SIGNAL("pressed()"),
+            self.btnUseDefaults_clicked)
+        QtCore.QObject.connect(
+            self.ui.btnUseDriver,
+            QtCore.SIGNAL("pressed()"),
+            self.btnUseDriver_clicked)
+        QtCore.QObject.connect(
+            self.ui.btnCancel,
+            QtCore.SIGNAL("pressed()"),
+            self.btnCancel_clicked)
+        QtCore.QObject.connect(
+            self.ui.btnMoreInfo,
+            QtCore.SIGNAL("pressed()"),
+            self.btnMoreInfo_clicked)
 
     def btnUseDefaults_clicked(self):
         self.user_result = "DEFAULT"
