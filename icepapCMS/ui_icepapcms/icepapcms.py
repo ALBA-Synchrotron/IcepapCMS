52af767431c78a03f2a5fd30730093d7e5d1e10d
Roberto Javier Homs Puron <rhoms@cells.es>
2020-04-23 09:07:11 +0200

    Reorganize entry point

--- icepapCMS/ui_icepapcms/icepapcms.py
+++ icepapCMS/ui_icepapcms/icepapcms.py
@@ -1131,8 +1131,22 @@ class IcepapCMS(QtGui.QMainWindow):
             self.ui.dockTree.close()
 
 
+def main():
+    args = sys.argv
+    args[0] = os.path.abspath(__file__)
+
+    # Check env. var.
+    ldap = os.environ.get('ICEPAP_LDAP', "")
+    all_networks = os.environ.get('ICEPAP_ALL_NETWORKS', "")
+
+    if ldap.lower() == "yes":
+        args.append("--ldap")
+
+    if all_networks.lower() == "yes":
+        args.append("--all-networks")
+
+    app = IcepapApp(args)
+
+
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    ipapcfg = IcepapCMS()
-    ipapcfg.show()
-    sys.exit(app.exec_())
+    main()
