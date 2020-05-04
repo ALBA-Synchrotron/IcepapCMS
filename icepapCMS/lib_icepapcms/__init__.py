c0ee59ab9bd0bf60ac3b09e50803a0043f21ae80
Roberto Javier Homs Puron <rhoms@cells.es>
2020-05-04 18:46:08 +0200

    Remove unused code

--- icepapCMS/lib_icepapcms/__init__.py
+++ icepapCMS/lib_icepapcms/__init__.py
@@ -3,6 +3,5 @@ from .icepapsystem import *
 from .icepapdriver import *
 from .conflict import Conflict
 from .configmanager import ConfigManager
-from .timer import Timer
 from .icepapcontroller import *
 from .stormmanager import *
