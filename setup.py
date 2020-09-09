6b686d46be850863d31648392e087e878dc8279f
Roberto Javier Homs Puron <rhoms@cells.es>
2020-09-09 12:08:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,19 @@
 from setuptools import setup, find_packages
 
 
-long_description = """IcepapCfg is a configuration and test tool for the
-Icepap motor controller"""
+long_description = """
+IcepapCMS is a graphical configuration and test tool for the Icepap motor 
+controller, base on icepap library. 
+
+It allows to configure the electrical parameters, I/O interfaces, encoders, 
+movement parameters, homing procedures. 
+
+All changes are saved on a data base which allows to do easy rollbacks, 
+identified changes and conflicts between the hardware configuration and the 
+data base configuration. 
+
+The program includes on console to communicate with raw commands.  
+"""
 
 # The version is updated automatically with bumpversion
 # Do not update manually
@@ -18,22 +29,21 @@ setup(
     packages=find_packages(),
     description="Icepap Configuration Management System and Test Tool",
     long_description=long_description,
-    author="Guifre Cuni",
-    author_email="guifre.cuni@cells.es",
+    author="Guifre Cuni et al.",
+    author_email="ctbeamlines@cells.es",
     entry_points={
         'gui_scripts': [
             'icepapcms = icepapCMS.__main__:main',
             'ipapconsole = icepapCMS.ui_icepapcms.ipapconsole:main'],
     },
-    # install_requires=[
-    #     "storm==0.20",
-    #     "IPy>=0.62",
-    #     "PyQt4",
-    #      MySQL-python
-    #     "sqlite3", # or pysqlite2 (not in pypi)
-    #     "pyicepap" #not in pypi
-    #     "ldap" # needed by ldap_login (ALBA-Cells library)
-    # ],
+    install_requires=[
+        "storm>=0.23",
+        "IPy>=0.62",
+        "PyQt5",
+        "icepap",
+        'configobj',
+
+    ],
     include_package_data=True,
     platforms=["Linux, Windows XP/Vista/7/8"],
     url="http://computing.cells.es/products/icepap-cms"
