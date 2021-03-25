1943e932029b7a29846ed18160e1142635584327
Roberto Javier Homs Puron <rhoms@cells.es>
2021-03-25 16:43:16 +0100

    Bump version 3.0.1 to 3.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ The program includes one graphical console to communicate with raw commands.
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '3.0.1'
+__version = '3.1.0'
 
 
 # Setup
